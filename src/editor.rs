6e566ff52d7ecf3025e09cf01b896a8f700c6d97
viv-defy <vishnu@MacBook-Pro.local>
2023-12-29 12:42:04 +0530

    used clippy to make idiomatic changes

--- src/editor.rs
+++ src/editor.rs
@@ -20,18 +20,18 @@ impl Editor {
                         }
                     }
                     Key::Ctrl('q') => break,
-                    _ => println!("{:?}\r", key),
+                    _ => println!("{key:?}\r"),
                 },
-                Err(err) => die(err),
+                Err(err) => die(&err),
             }
         }
     }
 
     pub fn default() -> Self {
-        Editor {}
+        Self {}
     }
 }
 
-fn die(e: std::io::Error) {
+fn die(e: &std::io::Error) {
     panic!("{e}");
 }
